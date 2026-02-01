6ed6e733a664a9b9eb72cc24be48d6c4b1de522c
Sy Le <lenguyensy@gmail.com>
2026-02-01 09:04:32 -0800

    clean up script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 sh format.sh
 
 echo 'Download Import Map JSON'
-curl -o import-map.json https://synle.github.io/nav-generator/import-map.json
+curl -s -S -o import-map.json https://synle.github.io/nav-generator/import-map.json
 
 echo 'Download index.html template'
-curl -o index.html https://synle.github.io/nav-generator/index.template.html
+curl -s -S -o index.html https://synle.github.io/nav-generator/index.template.html
