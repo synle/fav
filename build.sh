fe5346d189370675ce81266dc8e00a12696304ad
Sy Le <lenguyensy@gmail.com>
2026-02-07 11:13:10 -0800

    clean up build and use reuseable build now
    CI / CD - Prettier Automatic Commit
    
    Update url-porter.json
    
    CI / CD - Prettier Automatic Commit

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
-sh format.sh
-
 echo 'Download index.html template'
 curl -s -S -o index.html https://synle.github.io/nav-generator/index.template.html
 
+echo 'Download dev.sh template'
+curl -s -S -o dev.sh https://synle.github.io/nav-generator/dev.sh
+
 echo 'Build URL Porter JSON'
 node url-porter.build.js
+
+npm run format
